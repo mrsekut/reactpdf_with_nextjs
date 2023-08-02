8e0c6e9bb382790b8ed10956c7c4bdbb58493531
mrsekut <k.cloudspider@gmail.com>
2023-08-02 17:52:25 +0900

    childの中でfetchできる

--- app/sample.tsx
+++ app/sample.tsx
@@ -3,25 +3,36 @@ import { Page, View, Text } from "@react-pdf/renderer";
 import React, { useEffect, useState } from "react";
 
 export const Sample: React.FC = () => {
-	const data = useFetchData("https://jsonplaceholder.typicode.com/posts/1");
-
 	return (
 		<Page size="A4" orientation="landscape">
 			<View>
-				<Text>Section #1</Text>
-				{data && <Text>Data: {JSON.stringify(data)}</Text>}
+				<Text>parent</Text>
+				<Child />
 			</View>
 		</Page>
 	);
 };
 
-export const useFetchData = (url: string) => {
+const Child: React.FC = () => {
+	const data = useFetchData("https://jsonplaceholder.typicode.com/posts/1");
+
+	return (
+		<View>
+			<Text>child</Text>
+			{data && <Text>Data: {JSON.stringify(data)}</Text>}
+		</View>
+	);
+};
+
+const useFetchData = (url: string) => {
 	const [data, setData] = useState(null);
 
 	useEffect(() => {
 		fetch(url)
 			.then((response) => response.json())
-			.then((json) => setData(json));
+			.then((json) => {
+				setTimeout(() => setData(json), 3000);
+			});
 	}, [url]);
 
 	return data;
