2536b9253686b5e225306650124658489986dc3b
mrsekut <k.cloudspider@gmail.com>
2023-08-02 17:43:07 +0900

    pdfを表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 "use client";
+import { PDFViewer, Document } from "@react-pdf/renderer";
+import { Sample } from "./sample";
 
 export default function Home() {
-	return <main className="min-h-screen bg-red-100">ooo</main>;
+	return (
+		<main className="min-h-screen bg-red-100 p-10">
+			<PDFViewer className="min-h-[90vh] w-full">
+				<Document>
+					<Sample />
+				</Document>
+			</PDFViewer>
+		</main>
+	);
 }
